57399198199be2f2052916bba4533470c08b0353
Ezequiel Esnaola <eze.esnaola@gmail.com>
2019-05-08 21:14:20 -0300

    Change rss url

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
 
         //Change this to your own rss feeds
-        "http://feeds.feedburner.com/phpToday",
+        "https://dev.to/feed/eesnaola",
 
         {
         // how many entries do you want?
